262801e1d9c3737bd42ccad33f7d38ee40d12c04
Victor Najar <vand97@gmail.com>
2017-08-27 11:22:02 -0500

    last

--- assets/js/init.js
+++ assets/js/init.js
@@ -15,7 +15,7 @@ var password = '1234';
 
 $( "#submit" ).click(function() {
   if($("#user").val() === user && $('#password').val() === password){
-    window.location = 'examples/login.html'; //url de archivo
+    window.location = '../examples/opcion.html'; //url de archivo
   } else{
     Materialize.toast('Usuario o contraseña incorrecto', 4000);
   }
