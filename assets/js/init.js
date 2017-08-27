e78f6e3b9bf800b935160316e38786368c1eef08
Victor Najar <vand97@gmail.com>
2017-08-27 01:03:14 -0500

    Modify Victor

---
+++ assets/js/init.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
+var user = 'NuevoLeon';
+var password = '1234';
+
+$( "#submit" ).click(function() {
+  if($("#user").val() === user && $('#password').val() === password){
+    window.location = 'examples/login.html'; //url de archivo
+  } else{
+    Materialize.toast('Usuario o contraseña incorrecto', 4000);
+  }
+});
